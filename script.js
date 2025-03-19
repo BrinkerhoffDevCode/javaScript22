c3c447b7610bb4d67b483650badd6524df1fd605
allysonschaedlerbrinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-03-19 09:31:56 -0300

    ADD - Commit Inicail

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function VerificarPar(numero){
+    if(numero % 2 === 0) {
+        console.log('par');
+    }else{
+        console.log('impar');
+    }
+}
+
+VerificarPar(4);
+VerificarPar(10);
