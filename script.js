003a0b32dfa23d3fed866fcae29c5be5b7942ac2
allysonschaedlerbrinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-03-19 10:08:09 -0300

    UPDATE- teste 1

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ function VerificarPar(numero){
 
 VerificarPar(4);
 VerificarPar(10);
+
+//let numeroUsuario = parseInt(prompt("Digite um número:"));
+//VerificarPar(numeroUsuario);
+
+
+
+function Calcular(n1,n2){
+    const soma= n1 + n2;
+    const subtracao= n1 - n2;
+    const divisao= n2 !== 0 ? n1/n2: 'Não pode dividir por zero';
+    const multiplicacao= n1 * n2;
+    
+    console.log(soma);
+    console.log(subtracao);
+    console.log(divisao);
+    console.log(multiplicacao);
+}
+
+//let n1 = parseFloat(prompt("Digite seu primeiro número:"));
+//let n2 = parseFloat(prompt("Digite seu primeiro número:"));
+
+Calcular(n1,n2);
+
+let numero = 10;
+let delay = 0;
+
+while (numero >=1){
+    
+    console.log(numero);
+    numero--;
+
+}
+   
