b3628f233b98b92f2c977d62c772ceb9ac979d68
BrinkerhoffDevCode <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-06-03 07:37:50 -0300

    Update - Continuação de mais tres questões

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-function VerificarPar(numero){
-    if(numero % 2 === 0) {
+function VerificarPar(numero) {
+    if (numero % 2 === 0) {
         console.log('par');
-    }else{
+    } else {
         console.log('impar');
     }
 }
@@ -15,12 +15,12 @@ VerificarPar(10);
 
 
 
-function Calcular(n1,n2){
-    const soma= n1 + n2;
-    const subtracao= n1 - n2;
-    const divisao= n2 !== 0 ? n1/n2: 'Não pode dividir por zero';
-    const multiplicacao= n1 * n2;
-    
+function Calcular(n1, n2) {
+    const soma = n1 + n2;
+    const subtracao = n1 - n2;
+    const divisao = n2 !== 0 ? n1 / n2 : 'Não pode dividir por zero';
+    const multiplicacao = n1 * n2;
+
     console.log(soma);
     console.log(subtracao);
     console.log(divisao);
@@ -30,15 +30,22 @@ function Calcular(n1,n2){
 //let n1 = parseFloat(prompt("Digite seu primeiro número:"));
 //let n2 = parseFloat(prompt("Digite seu primeiro número:"));
 
-Calcular(n1,n2);
+Calcular(n1, n2);
 
 let numero = 10;
-let delay = 0;
-
-while (numero >=1){
-    
+while (numero >= 1) {
     console.log(numero);
     numero--;
-
 }
-   
+
+function inverterTexto() {
+    texto = prompt("digite uma palavra");
+    return texto.split('').reverse().join('');
+}
+
+inverterTexto();
+
+function contarCaracteres(texto) {
+    return texto.length;
+}
+
